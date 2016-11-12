73c177562827d91d7c4c73232fff84770f49b573
Amir Abrams <AmirAbrams@users.noreply.github.com>
2016-11-12 17:43:35 -0600

    list_of -> boost::assign::list_of

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -86,11 +86,11 @@ public:
         vSeeds.push_back(CDNSSeedData("bitcointx.io", "btxseed6.bitcointx.io"));
         vSeeds.push_back(CDNSSeedData("bitcointx.io", "btxseed7.bitcointx.io"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of(35);
-        base58Prefixes[SCRIPT_ADDRESS] = list_of(85);
-        base58Prefixes[SECRET_KEY] =     list_of(153);
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
+        base58Prefixes[PUBKEY_ADDRESS] = boost::assign::list_of(35);
+        base58Prefixes[SCRIPT_ADDRESS] = boost::assign::list_of(85);
+        base58Prefixes[SECRET_KEY] =     boost::assign::list_of(153);
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E);
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4);
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
