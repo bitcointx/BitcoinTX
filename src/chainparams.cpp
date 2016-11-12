d3efaf7a8da6fe3c866bd0affd6471a5b2515121
Amir Abrams <AmirAbrams@users.noreply.github.com>
2016-11-12 17:55:44 -0600

    [Params] Add convert_to_container for list_of
    Add convert_to_container so boost::assign::list_of becomes vector<unsigned char>

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -89,8 +89,8 @@ public:
         base58Prefixes[PUBKEY_ADDRESS] = boost::assign::list_of(35);
         base58Prefixes[SCRIPT_ADDRESS] = boost::assign::list_of(85);
         base58Prefixes[SECRET_KEY] =     boost::assign::list_of(153);
-        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E);
-        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4);
+        base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+        base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
 
